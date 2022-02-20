79fbc3888ce8f4ce8f85de8ba41df657c437f80d
dach83 <dach83@mail.ru>
2022-02-20 15:07:50 +0500

    Convert MainActivity to Kotlin

--- app/src/main/java/com/example/gb04_android_on_kotlin_start/MainActivity.kt
+++ app/src/main/java/com/example/gb04_android_on_kotlin_start/MainActivity.kt
@@ -1,14 +1,11 @@
-package com.example.gb04_android_on_kotlin_start;
+package com.example.gb04_android_on_kotlin_start
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
