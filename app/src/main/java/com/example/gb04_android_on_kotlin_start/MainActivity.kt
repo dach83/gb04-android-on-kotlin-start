dd85335cca534ababd87d66196df704a664b2284
dach83 <dach83@mail.ru>
2022-02-20 15:14:43 +0500

    Added button to MainActivity

--- app/src/main/java/com/example/gb04_android_on_kotlin_start/MainActivity.kt
+++ app/src/main/java/com/example/gb04_android_on_kotlin_start/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.gb04_android_on_kotlin_start
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var clickMeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        clickMeButton = findViewById<Button>(R.id.click_me_button)
+        clickMeButton.setOnClickListener {
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
